58eacfda54efb6496723b34c43bef1a5642d8299
mmoore <mmoore@oucs1375.otago.ac.nz>
2018-05-17 20:44:32 +1200

    worked on regex's

--- Etude05/dateValidator.java
+++ Etude05/dateValidator.java
@@ -6,9 +6,26 @@ class dateValidator implements validator {
 
 
     private static Pattern pattern1 = Pattern.compile(
-            //"((?!0)\\d|0(?!0)\\d|1\\d|3(?![2-9])\\d)"
-            "
-            ///?   (0(?!2|4|6|9)\\d|1(?!1|3|4|5|6|7|8|9)\\d)  /?  (19[5-9][0-9]|20[0-4][0-9])"
+            //pattern 1
+            
+          // "(((?!0)\\d|0(?!0)\\d|1\\d|2\\d|30|31)(/|-)((?!0|2|4|6|9)\\d|0(?!0|2|4|6|9)\\d|10|12|Jan|Mar|May|Jul|Aug|Oct|Dec)(/|-)((17|18|19|2[0-9])[0-9]{2}|3000|(0\\d|\\d\\d)))"
+            //pattern 2
+            "|(((?!0)\\d|0(?!0)\\d|1\\d|2\\d|30)(/|-)((4|6|9|)|0(4|6|9)|11|Apr|Jun|Sep|Nov)(/|-)((17|18|19|2[0-9])[0-9]{2}|3000)|(0\\d|\\d\\d)))"
+
+            //pattern 3
+           + "|(((?!0)\\d|0(?!0)\\d|1\\d|2(?!9)\\d)(/|-)(2|02|FEB)(/|-)(((17|18|19|2[0-9])[0-9]{2}|3000)|(0\\d|\\d\\d)))"
+            
+            //months for pattern 1
+            //"((?!0|2|4|6|9)\\d|0(?!0|2|4|6|9)\\d|10|12|Jan|Mar|May|Jul|Aug|Oct|Dec)"
+
+            //months for pattern 2
+            //"((4|6|9|)|0(4|6|9)|11|Apr|Jun|Sep|Nov)"
+
+            //months for pattern 3
+            //"((2|02|FEB)"
+            
+          //  "((2000|2400|2800|(19|2[0-9](0[48]|[2468][048]|[13579][26]))))" 
+
             );
     private static Pattern pattern2 = Pattern.compile("0");
     private static Pattern pattern3 = Pattern.compile("0");
@@ -27,16 +44,21 @@ class dateValidator implements validator {
         }
 
 
-        if     (month == "01" || month == "03" || month == "05" || month == "07" || month == "08" || month == "10" || month == "12" ||
-                month == "1"  || month == "3"  || month == "5"  || month == "7"  ||  month == "8" ||
+             if(month == "01" || month == "03" || month == "05" || 
+                month == "07" || month == "08" || month == "10" || 
+                month == "12" || month == "1"  || month == "3"  || 
+                month == "5"  || month == "7"  || month == "8"  ||
+                month == "Jan"|| month == "Mar"|| month == "May"|| 
+                month == "Jul"|| month == "Aug"|| month == "Oct"|| 
+                month == "Dec")
 
-                month == "Jan" || month == "Mar" || month == "May" || month == "Jul" || month == "Aug" || month == "Oct" || month == "Dec")
         {return 1;}
 
-        else if(month == "04" || month == "06" || month == "09" || month == "11" ||
-                month == "4"  || month == "6"  || month == "9"  ||
+        else if(month == "04" || month == "06" || month == "09" || 
+                month == "11" || month == "4"  || month == "6"  || 
+                month == "9"  || month == "Apr"|| month == "Jun"|| 
+                month == "Sep"|| month == "Nov")
 
-                month == "Apr" || month == "Jun" || month == "Sep" || month == "Nov")
         {return 2;}
 
         else

12c10d522b6411898916aa98d2de52ed1d65edb4
mmoore <mmoore@oucs1429.otago.ac.nz>
2018-05-21 22:36:43 +1200

    Made changes to dates

--- Etude05/dateValidator.java
+++ Etude05/dateValidator.java
@@ -2,35 +2,123 @@ import java.util.regex.Pattern;
 import java.util.Scanner;
 import java.lang.System;
 
-class dateValidator implements validator {
+class dateValidator{
+
+    private static Pattern days31 = Pattern.compile
+        ("((?!0)\\d|0(?!0)\\d|1\\d|2\\d|30|31)");
+
+    private static Pattern months31 = Pattern.compile
+        ("((?!0|2|4|6|9)\\d|0(?!0|2|4|6|9)\\d|10|12|Jan|Mar|May|Jul|Aug|Oct|Dec)");
+
+    private static Pattern days30 = Pattern.compile
+        ("((?!0)\\d|0(?!0)\\d|1\\d|2\\d|30)");
+
+    private static Pattern months30 = Pattern.compile
+        ("((4|6|9|)|0(4|6|9)|11|Apr|Jun|Sep|Nov)");
+
+    private static Pattern daysFebNLY = Pattern.compile
+        ("((?!0)\\d|0(?!0)\\d|1\\d|2(?!9)\\d)");
+
+    private static Pattern daysFebLY = Pattern.compile
+        ("((?!0)\\d|0(?!0)\\d|1\\d|2\\d)");
+
+    private static Pattern feb = Pattern.compile
+        ("(2|02|FEB)");
+
+    private static Pattern years = Pattern.compile 
+        ("(17|18|19|2[0-9])[0-9]{2}|3000|(0\\d|\\d\\d)");
+
+    private static Pattern leapYears = Pattern.compile
+        ("(2000|2400|2800|3000|((18|19|2[0-9])(0[48]|[2468][048]|[13579][26])|(17([68][048]|(56|[79][26])))))"); 
+
+    public static String seperatorError = "seperator error, seperator format invalid";
 
 
-    private static Pattern pattern1 = Pattern.compile(
-            //pattern 1
-            
-          // "(((?!0)\\d|0(?!0)\\d|1\\d|2\\d|30|31)(/|-)((?!0|2|4|6|9)\\d|0(?!0|2|4|6|9)\\d|10|12|Jan|Mar|May|Jul|Aug|Oct|Dec)(/|-)((17|18|19|2[0-9])[0-9]{2}|3000|(0\\d|\\d\\d)))"
-            //pattern 2
-            "|(((?!0)\\d|0(?!0)\\d|1\\d|2\\d|30)(/|-)((4|6|9|)|0(4|6|9)|11|Apr|Jun|Sep|Nov)(/|-)((17|18|19|2[0-9])[0-9]{2}|3000)|(0\\d|\\d\\d)))"
+   public String[] splitDate(String date){
+   
+   String[] splitDate = new String[3];
+   String year = "";
+   String month = "";
+   String day = "";
+    
+    if(date.contains ("/")){
+            year = date.substring(date.indexOf("/")+1);
+            year = year.substring(year.indexOf("/")+1);
 
-            //pattern 3
-           + "|(((?!0)\\d|0(?!0)\\d|1\\d|2(?!9)\\d)(/|-)(2|02|FEB)(/|-)(((17|18|19|2[0-9])[0-9]{2}|3000)|(0\\d|\\d\\d)))"
-            
-            //months for pattern 1
-            //"((?!0|2|4|6|9)\\d|0(?!0|2|4|6|9)\\d|10|12|Jan|Mar|May|Jul|Aug|Oct|Dec)"
+        }else if(date.contains("-")){
+            year = date.substring(date.indexOf("-")+1);
+            year = year.substring(year.indexOf("-")+1);
 
-            //months for pattern 2
-            //"((4|6|9|)|0(4|6|9)|11|Apr|Jun|Sep|Nov)"
+        }else if(date.contains(" ")){
+            year = date.substring(date.indexOf(" ")+1);
+            year = year.substring(year.indexOf(" ")+1);
+}else{
+year = seperatorError;
 
-            //months for pattern 3
-            //"((2|02|FEB)"
-            
-          //  "((2000|2400|2800|(19|2[0-9](0[48]|[2468][048]|[13579][26]))))" 
+splitDate[2] = year;
 
-            );
-    private static Pattern pattern2 = Pattern.compile("0");
-    private static Pattern pattern3 = Pattern.compile("0");
-    public int checkMonth(String date){
+    if(date.contains ("/")){
+            month = date.substring(date.indexOf("/")+1);
+            month = month.substring(0, month.indexOf("/"));
 
+        }else if(date.contains("-")){
+            month = date.substring(date.indexOf("-")+1);
+            month = month.substring(0, month.indexOf("-"));
+
+        }else if(date.contains(" ")){
+            month = date.substring(date.indexOf(" ")+1);
+            month = month.substring(0, month.indexOf(" "));
+        }
+
+        month = month.toUpperCase();
+        splitDate[1] = month;
+
+        if(date.contains("/")){
+            day = date.substring(0, date.indexOf("/"));
+            day = day.substring(0,date.indexOf("/"));
+
+        }else if(date.contains("/")){
+            day = date.substring(0, date.indexOf("-"));
+            day = day.substring(0,date.indexOf("-"));
+
+        }else if(date.contains("/")){
+            day = date.substring(0, date.indexOf("-"));
+            day = day.substring(0,date.indexOf("-"));
+}
+
+        splitDate[0] = day;
+        }
+        return splitDate;
+    }
+
+
+
+
+    
+
+    public String getYear(String date){
+        String year = "";
+
+        if(date.contains ("/")){
+            year = date.substring(date.indexOf("/")+1);
+            year = year.substring(year.indexOf("/")+1);
+
+        }else if(date.contains("-")){
+            year = date.substring(date.indexOf("-")+1);
+            year = year.substring(year.indexOf("-")+1);
+
+        }else if(date.contains(" ")){
+            year = date.substring(date.indexOf(" ")+1);
+            year = year.substring(year.indexOf(" ")+1);
+
+        }else{
+            year = seperatorError;
+
+            }
+            return year;
+    }
+
+    public String getMonth(String date){
         String month = "";
 
         if(date.contains ("/")){
@@ -38,52 +126,114 @@ class dateValidator implements validator {
             month = month.substring(0, month.indexOf("/"));
 
         }else if(date.contains("-")){
-
             month = date.substring(date.indexOf("-")+1);
             month = month.substring(0, month.indexOf("-"));
+
+        }else if(date.contains(" ")){
+            month = date.substring(date.indexOf(" ")+1);
+            month = month.substring(0, month.indexOf(" "));
         }
 
+        month = month.toUpperCase();
+        return month;
+    }
 
-             if(month == "01" || month == "03" || month == "05" || 
-                month == "07" || month == "08" || month == "10" || 
-                month == "12" || month == "1"  || month == "3"  || 
-                month == "5"  || month == "7"  || month == "8"  ||
-                month == "Jan"|| month == "Mar"|| month == "May"|| 
-                month == "Jul"|| month == "Aug"|| month == "Oct"|| 
-                month == "Dec")
+    public String getDay(String date){
+        String day = "";
 
+        if(date.contains("/")){
+            day = date.substring(0, date.indexOf("/"));
+            day = day.substring(0,date.indexOf("/"));
+
+        }else if(date.contains("/")){
+            day = date.substring(0, date.indexOf("-"));
+            day = day.substring(0,date.indexOf("-"));
+
+        }else if(date.contains("/")){
+            day = date.substring(0, date.indexOf("-"));
+            day = day.substring(0,date.indexOf("-"));
+}
+        return day;
+    }
+
+/*
+    public int checkYear(String year){
+
+        if (Integer.parseInt(year) % 4 == 0 && Integer.parseInt(year) % 100 !=0)
         {return 1;}
 
-        else if(month == "04" || month == "06" || month == "09" || 
-                month == "11" || month == "4"  || month == "6"  || 
-                month == "9"  || month == "Apr"|| month == "Jun"|| 
-                month == "Sep"|| month == "Nov")
-
-        {return 2;}
+        else if (Integer.parseInt(year) % 400 == 0)
+        {return 1;}
 
         else
-
-        {return 3;}
+        {return 0;}
 
     }
 
+*/
 
-    @Override
-    public boolean matches(String date) {
+    /*
+       public int checkMonth(String date){
 
-       // if(checkMonth(date) == 1){
-            return pattern1.matcher(date).matches();
+       if(     month.equals("01") || month.equals("03") || month.equals("05") || 
+       month.equals("07") || month.equals("08") || month.equals("10") || 
+       month.equals("12") || month.equals("1")  || month.equals("3")  || 
+       month.equals("5")  || month.equals("7")  || month.equals("8")  ||
+       month.equals("JAN")|| month.equals("MAR")|| month.equals("MAY")|| 
+       month.equals("JUL")|| month.equals("AUG")|| month.equals("OCT")|| 
+       month.equals("DEC"))
 
-        //}else if(checkMonth(date) == 2){
-          //  return pattern2.matcher(date).matches();
+       {return 1;}
 
-        //}else if(checkMonth(date) == 3){
-        //    return pattern3.matcher(date).matches();
+       else if(month.equals("04") || month.equals("06") || month.equals("09") || 
+       month.equals("11") || month.equals("4")  || month.equals("6")  || 
+       month.equals("9")  || month.equals("APR")|| month.equals("JUN")|| 
+       month.equals("SEP")|| month.equals("NOV"))
 
-        //}else{
-        //    System.out.println("Error");
-       // }
-        
-    //return false;
+       {return 2;}
+
+       else
+
+       {return 3;}
+
+       }
+
+*/
+
+    public boolean matchLeapYear(String year){
+        return leapYears.matcher(year).matches();
     }
+
+    public boolean matchYear(String year){
+        return years.matcher(year).matches();
+    }
+
+    public boolean matchFeb(String month){
+        return feb.matcher(month).matches();
+    }
+
+    public boolean matchMonths30(String month){
+        return months30.matcher(month).matches();
+    }
+
+    public boolean matchMonths31(String month){
+        return months31.matcher(month).matches();
+    }
+
+    public boolean matchDaysFebLY(String day){
+        return daysFebLY.matcher(day).matches();
+    }
+
+    public boolean matchDaysFebNLY(String day){
+        return daysFebNLY.matcher(day).matches();
+    }
+
+    public boolean matchDays30(String day){
+        return days30.matcher(day).matches();
+    }
+    public boolean matchDays31(String day){
+        return days31.matcher(day).matches();
+    }
+
+
 }
--- Etude05/dateValidatorApp.java
+++ Etude05/dateValidatorApp.java
@@ -10,31 +10,128 @@ public class dateValidatorApp {
 
         Scanner input = new Scanner(System.in);
         String date = "";
-        int trueCount = 0;
-        int falseCount= 0;
+        String year = "";
+        String month = "";
+        String day = "";
         dateValidator validator = new dateValidator();
+        String dayOutOfRange = "Invalid day entered, day out of range";
+        String monthOutOfRange = "Invalid month entered, month out of range";
+        String yearOutOfRange = "Invalid year entered, year out of range";
+        String dayInvalid = "Invalid day entered, day format is incorrect";
 
         while(true){
 
             date = input.nextLine();
 
+            year = validator.getYear(date);
+            month = validator.getMonth(date);
+            day = validator.getDay(date);
 
-            if(date.equals("exit")) {
+            if(validator.matchLeapYear(year) == true){
+                System.out.println("vaild leapyear");
+
+
+
+                if(validator.matchFeb(month) == true){
+                    System.out.println("valid feb");
+
+                    if(validator.matchDaysFebLY(day) == true){
+                        System.out.println("valid FebLY days");
+                    }else{
+                        System.out.println(dayOutOfRange);
+                    }
+
+
+                }else if(validator.matchMonths30(month) == true){
+                    System.out.println("valid 30day month");
+
+                    if(validator.matchDays30(day) == true){
+                        System.out.println("valid 30day");
+                    }else{
+                        if(day > 2){
+                         System.out.println(dayInvalid){
+                         else{
+                        System.out.println(dayOutOfRange);
+                        }
+                    }
+
+
+                }else if(validator.matchMonths31(month) == true){
+                    System.out.println("Valid 31day month");
+
+                    if(validator.matchDays31(day) == true){
+                        System.out.println("Valid 31day");
+                    }else{
+                        System.out.println(dayOutOfRange);
+                    }
+
+                }else{
+                    System.out.println(monthOutOfRange);
+                }
+
+
+
+            }else if(validator.matchYear(year) == true){
+                System.out.println("valid non-leapyear");
+
+                if(validator.matchFeb(month) == true){
+                    System.out.println("valid feb");
+
+                    if(validator.matchDaysFebNLY(day) == true){
+                        System.out.println("valid FebNLY days");
+                    }else{
+                        System.out.println(dayOutOfRange);
+                    }
+
+
+                }else if(validator.matchMonths30(month) == true){
+                    System.out.println("valid 30day month");
+
+                    if(validator.matchDays30(day) == true){
+                        System.out.println("valid 30day");
+                    }else{
+                        System.out.println(dayOutOfRange);
+                    }
+
+
+                }else if(validator.matchMonths31(month) == true){
+                    System.out.println("Valid 31day month");
+
+                    if(validator.matchDays31(day) == true){
+                        System.out.println("Valid 31day");
+                    }else{
+                        System.out.println(dayOutOfRange);
+                    }
+
+
+                }else{
+                    System.out.println(monthOutOfRange);
+                }
+
+                
+            }else if(year.equals(validator.seperatorError)){
+                System.out.println(validator.seperatorError);
+            }
+
+            if(date.equals("quit") || date.equals("q")) {
                 break;
             }
 
-            if (validator.matches(date) == true){
-                trueCount++;
-                System.out.println("Correct");
-            }
+            /*
 
-            if (validator.matches(date) == false){
-                falseCount++;
-                System.out.println("Wrong");
-                }
+               if (validator.matches(date) == true){
+               trueCount++;
+               System.out.println("Correct");
+               }
+
+               if (validator.matches(date) == false){
+               falseCount++;
+               System.out.println("Wrong");
+               }
+               */
         }
-        System.out.println(trueCount+" valid dates entered");
-        System.out.println(falseCount+" invalid dates entered");
+        //System.out.println(trueCount+" valid dates entered");
+        //System.out.println(falseCount+" invalid dates entered");
     }
 
 

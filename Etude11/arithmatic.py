e168111614714068dcefbd88375317433e29271a
mmoore <mmoore@oucs1440.otago.ac.nz>
2018-06-03 18:24:03 +1200

    Arithmatic is now working

--- Etude11/arithmatic.py
+++ Etude11/arithmatic.py
@@ -3,12 +3,10 @@ import shlex
 import os
 from argparse import ArgumentParser
 
-length = 0
-operators = []
-
 parser = ArgumentParser()
 parser.add_argument('-file', metavar='FILE', help='input a file using -file <filename>')
 args = parser.parse_args()
+targetFound = False
 
 with open(args.file) as file:
     inputs = []
@@ -16,22 +14,48 @@ with open(args.file) as file:
     for line in file:
         count += 1
         nextline = file.next()
-        values.append(shlex.split(line + nextline))
+        inputs.append(shlex.split(line + nextline))
 
-def leftToRight(find[], target, current, depth):
+def leftToRight(current, equation, depth):
+    global targetFound 
+    #print(equation)
+    if(targetFound):
+        return
+
+    elif(current != target and depth >= len(values) -1):
+        return
+
+    elif(current == target and depth >= len(values) -1):
+        finished = True
+        print("L" + str(target) + " "+ str(equation))
+    else:
+        depth = depth + 1
+        nextVal = values[depth]
+        leftToRight(values[depth] + current, equation + "+" + str(nextVal), depth)
+        leftToRight(values[depth] * current, equation + "*" + str(nextVal), depth)
+
+for i in range(len(inputs)):
+    values = []
+    targetFound = False
+    for j in range(len(inputs[i]) -2):
+        values.append(inputs[i][j])
+
+    values = list(map(int,values))
+
+    target = inputs[i][len(inputs[i]) -2]
+    target = int(target)
+
+    sign = inputs[i][len(inputs[i]) -1]
+
+    depth = 0
+    equation = ""
     
-    #Base case 
-    if(depth >= len(values) + 1) {
-        return False
-        }
+    if(sign is 'L'):
+        leftToRight(values[depth], str(values[depth]), depth)
+    elif(sign is 'N'):
+        print("Normal order not implemented")
+    else:
+        print("Incorrect input, please use 'L' or 'N' for the operation sign")
     
-    length = len(values[depth])
-
-    if(!((values[depth] + current) > target) or !((current * values[depth]) > target)):
-        
-        if(depth == len(values) - 1):
-            
-            if(current + values[depth] == target):
-
-
-
+"""
+Normal order is much more difficults to implement because we would have to consider BEDMAS order, effectily pushing the sign onto the equation, evaluating that equation and then poping that sign off if it was not the deired sign
